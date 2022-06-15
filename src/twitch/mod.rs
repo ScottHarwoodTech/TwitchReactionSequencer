f08c2cb378fcd5ed60dc42e110140d700edc22e1
Scott <scott@scottharwood.dev>
2022-06-16 00:23:36 +0100

    update

--- src/twitch/mod.rs
+++ src/twitch/mod.rs
@@ -1,6 +1,5 @@
-use twitch_irc::login::StaticLoginCredentials;
-use twitch_irc::TwitchIRCClient;
-use twitch_irc::{ClientConfig, SecureTCPTransport};
+use pubsub::Topic;
+use twitch_api2::pubsub;
 
 pub struct TwitchController {
     target_channel: &'static str,
@@ -14,24 +13,19 @@ impl TwitchController {
     }
 
     pub async fn start(&self) -> () {
-        // default configuration is to join chat as anonymous.
-        let config = ClientConfig::default();
-        let (mut incoming_messages, client) =
-            TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
-
-        // first thing you should do: start consuming incoming messages,
-        // otherwise they will back up.
-        let join_handle = tokio::spawn(async move {
-            while let Some(message) = incoming_messages.recv().await {
-                println!("Received message: {:?}", message);
-            }
-        });
-
-        // join a channel
-        // This function only returns an error if the passed channel login name is malformed,
-        // so in this simple case where the channel name is hardcoded we can ignore the potential
-        // error with `unwrap`.
-        client.join(self.target_channel.to_owned()).unwrap();
-        return join_handle.await.unwrap();
+        // We want to subscribe to moderator actions on channel with id 1234
+        // as if we were a user with id 4321 that is moderator on the channel.
+        let chat_mod_actions =
+            pubsub::channel_points::ChannelPointsChannelV1 { channel_id: 1234 }.into_topic();
+        // Create the topic command to send to twitch
+        let command = pubsub::listen_command(
+            &[chat_mod_actions],
+            "authtoken",
+            "super se3re7 random string",
+        )
+        .expect("serializing failed");
+        // Send the message with your favorite websocket client
+        send_command(command).unwrap();
+        // To parse the websocket messages, use pubsub::Response::parse
     }
 }
